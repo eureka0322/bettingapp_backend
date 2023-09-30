8d6527627e984d4b189c44161a176525cbbff528
LiveDream0322 <livedream0322@gmail.com>
2023-09-30 10:13:38 -0400

    change to https

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const betevoRoutes = require("./routes/betevoRoutes");
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 
@@ -11,11 +13,24 @@ app.use(express.json());
 
 app.use("/api", betevoRoutes);
 
+
+
+
 mongoose
   .connect("mongodb+srv://livedream0322:helloworld@cluster0.u7lyd63.mongodb.net/betting_db?retryWrites=true&w=majority")
   .then(() => {
-    app.listen(443, () => {
-      console.log("connected to db & listening on port", 443);
+    https
+    .createServer(
+      // Provide the private and public key to the server by reading each
+      // file's content with the readFileSync() method.
+      {
+        key: fs.readFileSync("key.pem"),
+        cert: fs.readFileSync("cert.pem"),
+      },
+      app
+    )
+    .listen(443, () => {
+      console.log("serever is runing at port 443");
     });
   })
   .catch((error) => {
